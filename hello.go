53eac3eb5d6db8a3e6d4e4ad131ec6632e4993c7
JoelKanyi <joelkanyi98@gmail.com>
2020-08-22 17:28:02 +0300

    some more practice

--- hello.go
+++ hello.go
@@ -2,8 +2,31 @@ package main
 
 import "fmt"
 
-
 func main() {
-    fmt.Println("hello"+"go")
-}
 
+	//Value types
+	fmt.Println("hello" + "go")
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
+	//Variables
+	var a = "Initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+
+}
